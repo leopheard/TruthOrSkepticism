6b1abb88b57737d7270e8389a3c629b42db215ef
leopheard <leopheard.misc@gmail.com>
2020-07-06 17:38:02 -0400

    upload

--- addon.py
+++ addon.py
@@ -7,6 +7,11 @@ url1 = "https://tastytradeloud.libsyn.com/rss"
 def main_menu():
     items = [
         {
+            'label': plugin.get_string(30002), 
+            'path': "https://bcovlive-a.akamaihd.net/r8ceb94e3229b4c0bb2dd461dacb3ab07/us-east-1/us-east-1/6057994532001/profile_2/c5s2x2oq_7ff6bd10c4804cf19626a72c71dd9994_media_162885.ts",
+            'thumbnail': "resources/media/icon.jpg", 
+            'is_playable': True},
+        {
             'label': plugin.get_string(30001), 
             'path': plugin.url_for('episodes1'),
             'thumbnail': "https://ssl-static.libsyn.com/p/assets/b/4/d/9/b4d95ef6e81d4e2a/TT1289_POD02.png"},
